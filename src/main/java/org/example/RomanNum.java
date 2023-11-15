ef013bb44cbeff1d1eec8daadb1a82033fed4d85
Bianca Andrei <BAndrei@spartaglobal.com>
2023-11-15 16:35:54 +0000

    Modifications to the solution

--- src/main/java/org/example/RomanNum.java
+++ src/main/java/org/example/RomanNum.java
@@ -6,13 +6,13 @@ public class RomanNum {
         }
 
         int value[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] numeral = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String[] symbol = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
         String result = "";
 
         for (int i = 0; i < value.length; i++){
             while(number >= value[i]){
-                result += numeral[i];
+                result += symbol[i];
                 number -= value[i];
             }
         }

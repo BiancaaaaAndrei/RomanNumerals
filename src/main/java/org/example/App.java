b788d2adf475020255494f0d0a03cd510fe18515
Bianca Andrei <BAndrei@spartaglobal.com>
2023-11-15 16:10:35 +0000

    Solution

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+public class App{
+    public static void main(String[] args) {
+        System.out.println(RomanNum.toRoman(4));
+        System.out.println(RomanNum.toRoman(16));
+        System.out.println(RomanNum.toRoman(100));
+        System.out.println(RomanNum.toRoman(3999));
+        System.out.println(RomanNum.toRoman(465));
+        System.out.println(RomanNum.toRoman(924));
+    }
+}
---
+++ src/main/java/org/example/RomanNum.java
@@ -0,0 +1,21 @@
+package org.example;
+public class RomanNum {
+    public static String toRoman(int number) {
+        if (number < 1 || number > 3999) {
+            System.out.println("Number must be between 1 and 3999");
+        }
+
+        int value[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] numeral = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        String result = "";
+
+        for (int i = 0; i < value.length; i++){
+            while(number >= value[i]){
+                result += numeral[i];
+                number -= value[i];
+            }
+        }
+        return result;
+    }
+}
